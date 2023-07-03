8597c84ccfad2d3fdcd882e0dd4a17aa6719ae68
NyleLuza <135330203+NyleLuza@users.noreply.github.com>
2023-07-02 20:50:49 -0700

    Initial commit

---
+++ app/src/main/java/AceTheJavaCodingInterview/module2_data_structures/ClassTemplate.java
@@ -0,0 +1,24 @@
+/**
+ * ClassTemplate Class
+ *
+ * <p>
+ *
+ * @question Problem Statement -
+ * @example
+ * @note Solution -
+ * @note Time Complexity -
+ * @note Space Complexity -
+ * @author David Kariuki
+ * @since /8/2022
+ */
+package AceTheJavaCodingInterview.module2_data_structures;
+
+public class ClassTemplate {
+
+  /**
+   * Main method
+   *
+   * @param args - String[]
+   */
+  public static void main(String[] args) {}
+}
